041067ce659d6877391e947ee41e4a9269b59f32
Darwin Zhang <darwindtz@hotmail.com>
2025-12-09 21:02:24 -0800

    Initialize test folder and test file for part 1

---
+++ tests/test_part_1.py
@@ -0,0 +1,12 @@
+"""
+Test Cases for Part 1 (Extraction)
+
+Test coverage:
+    - Normal asserts to make sure the function is working
+    - Edge cases of missing data, empty content, or non-HTML standard strings
+    - Special cases of non readable characters and whitespaces 
+
+# Author: Darwin Zhang
+# Date: 2025-12-09
+"""
+
