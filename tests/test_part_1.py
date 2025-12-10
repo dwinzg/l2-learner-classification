e726af0a5bd33105670a56888251a0abcd1755f9
Darwin Zhang <darwindtz@hotmail.com>
2025-12-09 22:25:35 -0800

    Test cases completed

--- tests/test_part_1.py
+++ tests/test_part_1.py
@@ -27,20 +27,81 @@ class TestExtraction(unittest.TestCase):
         html = "<html><li class='speaking'>Spanish</li></html>"
         soup = BeautifulSoup(html, 'html.parser')
         result = extract_l1(soup)
+        self.assertEqual(result, "Spanish")
+    
+    def test_extract_l1_missing(self):
+        # Missing L1, for edge cases 
+        html = "<html><body></body></html>"
+        soup = BeautifulSoup(html, 'html.parser')
+        result = extract_l1(soup)
+        self.assertIsNone(result)
 
+    def test_extract_l1_whitespace(self):
+        # If there is still whitespace
+        html = "<html><li class='speaking'>  Spanish  </li></html>"
+        soup = BeautifulSoup(html, 'html.parser')
+        result = extract_l1(soup)
         self.assertEqual(result, "Spanish")
 
     """
     Test 'extract_text'
     """
+    def test_extract_text_normal(self):
+        # Normal case
+        html = "<div id='body_show_ori'>Blah Blah Random text.</div>"
+        soup = BeautifulSoup(html, 'html.parser')
+        result = extract_text(soup)
+        self.assertEqual(result, "Blah Blah Random text.")
+
+    def test_extract_text_missing(self):
+        # No <dev>
+        html = "<html><body>Random text div</body></html>"
+        soup = BeautifulSoup(html, 'html.parser')
+        result = extract_text(soup)
+        self.assertEqual(result, "")
+
+    def test_extract_text_whitespace(self):
+        # For many white spaces 
+        html = "<div id='body_show_ori'>Many    white    space</div>"
+        soup = BeautifulSoup(html, 'html.parser')
+        result = extract_text(soup)
+        self.assertEqual(result, "Many white space")
 
     """
     Test 'parse_html'
     """
+    def test_parse_html_normal(self):
+        # Normal working case of splitting HTML, L1, and text 
+        html = """
+        <html>
+        <li class='speaking'>Spanish</li>
+        <div id='body_show_ori'>Given Text.</div>
+        </html>
+        """
+        l1, text, filename = parse_html(html, "test.html")
+        self.assertEqual(l1, "Spanish")
+        self.assertIn("Text", text)
+        self.assertEqual(filename, "test.html")
 
-    """
-    Test 'iterate_documents'
-    """
+    def test_parse_html_missing_l1(self):
+        # For missing case of L1, but text is present 
+        html = """
+        <html>
+        <div id='body_show_ori'>Given Another Text.</div>
+        </html>
+        """
+        l1, text, filename = parse_html(html, "test.html")
+        self.assertIsNone(l1)
+        self.assertIn("Another", text)
+        self.assertEqual(filename, "test.html")
+
+    def test_parse_html_empty(self):
+        # Empty HTML
+        html = """<html></html>"""
+        l1, text, filename = parse_html(html, "test.html")
+        self.assertIsNone(l1)
+        self.assertIn(text, '')
+        self.assertEqual(filename, "test.html")
 
 
 if __name__ == "__main__":
